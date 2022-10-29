acdcd2a8483abe0aa74b63ae2b3a5321d54ae586
voyager <cdspacenoob@gmail.com>
2022-10-29 22:22:37 +0900

    writeBoard @RequestParam

--- ProXML/src/main/java/kr/co/softcampus/controller/BoardController.java
+++ ProXML/src/main/java/kr/co/softcampus/controller/BoardController.java
@@ -1,15 +1,20 @@
 package kr.co.softcampus.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/board")	// 하위 주소를 설정해준다. "/board" 경로는 모두 여기를 거친다. 
 public class BoardController {
 
 	@GetMapping("/main")	// 사실상 요청을 받는 url은 "/board/main"이 된다. 
-	public String main() {
+	public String main(@RequestParam("board_info_idx") int board_info_idx, Model model) {
+		
+		model.addAttribute("board_info_idx", board_info_idx);
+		
 		return "board/main";	// return 할 때는 jsp의 절대 경로를 적어주는 것이므로 "board/main"을 다 적어준다. 
 	}
 	
