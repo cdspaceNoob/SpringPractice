cc97bd12dfc5ff630b4da9e0f4ec98c80f17433a
voyager <cdspacenoob@gmail.com>
2022-10-31 15:38:38 +0900

    file Upload

--- ProXML/src/main/java/kr/co/softcampus/service/BoardService.java
+++ ProXML/src/main/java/kr/co/softcampus/service/BoardService.java
@@ -1,15 +1,41 @@
 package kr.co.softcampus.service;
 
+import java.io.File;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import kr.co.softcampus.beans.ContentBean;
 
 @Service
 public class BoardService {
 
+	@Value("${path.upload}")
+	private String path_upload;
+	
+	private String saveUploadFile(MultipartFile upload_file) {
+		String file_name = System.currentTimeMillis() + "_" + upload_file.getOriginalFilename();	// 날짜 및 시각을 붙여서 중복되는 이름이 없도록.
+		
+		try {
+			upload_file.transferTo(new File(path_upload + "/" + file_name));
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		return file_name;
+	}
+	
 	public void addContentInfo(ContentBean writeContentBean) {
-		System.out.println(writeContentBean.getContent_subject());
-		System.out.println(writeContentBean.getContent_text());
-		System.out.println(writeContentBean.getUpload_file().getSize());
+//		System.out.println(writeContentBean.getContent_subject());
+//		System.out.println(writeContentBean.getContent_text());
+//		System.out.println(writeContentBean.getUpload_file().getSize());
+		
+		MultipartFile upload_file = writeContentBean.getUpload_file();
+		
+		if(upload_file.getSize() > 0) {
+			String file_name = saveUploadFile(upload_file);
+			System.out.println(file_name);
+		}
 	}
 }
